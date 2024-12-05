2e72eb9759b39c2dff907f93fc689b8f00f3db26
Catherine <catherine.dieval@hotmail.fr>
2024-12-05 18:36:08 +0100

    Fixed BoxWithStyledComponents.js

--- components/BoxWithStyledComponents.js
+++ components/BoxWithStyledComponents.js
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 
-export const StyledBox = styled.div`
+const StyledBox = styled.div`
   width: 100px;
   height: 100px;
-  background-color: ${(props) =>
-    props.$backgroundcolor === "black" ? "black" : "green"};
+  background-color: ${(props) => props.$isBlack ? "black" : "green"};
   margin: 2rem;
+
   &:hover {
     background-color: red;
   }
 `;
+
+export default function BoxWithStyledComponents({$isBlack}) {
+  return <StyledBox $isBlack={$isBlack} />;
+}
